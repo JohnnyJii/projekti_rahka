07a58d8a8a6563d38617f64fd5fa19f99687f868
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2018-11-26 13:50:33 +0200

    update

--- history/history.js
+++ history/history.js
@@ -20,23 +20,28 @@ window.onload = function () {
     title:{
       text: "Graafi"
     },
-    axisY:{
-      includeZero: false
+    axisX:{
+      title: "Date",
+      gridThickness: 2
+    },
+    axisY: {
+      title: "Meters"
     },
     data: [{
       type: "line",
       dataPoints: [
-        { y: 1700 },
-        { y: 1800 },
-        { y: 1900 },
-        { y: 1900 },
-        { y: 1800 },
-        { y: 1900 },
-        { y: 2200 },
-        { y: 2000 },
-        { y: 2200 },
-        { y: 2600 },
-        { y: 2500 }
+        { x: new Date(2000,0), y: 1500 },
+        { x: new Date(2000,1), y: 1800 },
+        { x: new Date(2000,2), y: 2100 },
+        { x: new Date(2000,3), y: 1900 },
+        { x: new Date(2000,4), y: 2500 },
+        { x: new Date(2000,5), y: 2300 },
+        { x: new Date(2000,6), y: 2620 },
+        { x: new Date(2000,7), y: 2600 },
+        { x: new Date(2000,8), y: 3000 },
+        { x: new Date(2000,9), y: 2800 },
+        { x: new Date(2000,10), y: 3100 },
+        { x: new Date(2000,11), y: 3800 }
       ]
     }]
   });

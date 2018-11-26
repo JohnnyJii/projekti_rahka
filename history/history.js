4215ebd9ce4f410864214b43ffe30a245189ea9f
Tuomas Myllyoja <tuomasmy@metropolia.fi>
2018-11-26 14:47:31 +0200

    t§

--- history/history.js
+++ history/history.js
@@ -12,30 +12,36 @@ sliderTrigger.addEventListener( "click" , function(el){
 
 });
 
- window.onload = function () {
+window.onload = function () {
 
-  const chart = new CanvasJS.Chart("chartContainer", {
+  let chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
     theme: "light2",
     title:{
       text: "Graafi"
     },
-    axisY:{
-      includeZero: false
+    axisX:{
+      title: "Date",
+      gridThickness: 2
+    },
+    axisY: {
+      title: "Meters"
     },
     data: [{
       type: "line",
       dataPoints: [
-        { y: 1600 },
-        { y: 1800 },
-        { y: 1900 },
-        { y: 1900 },
-        { y: 1800 },
-        { y: 1900 },
-        { y: 2200 },
-        { y: 2000 },
-        { y: 2200 },
-        { y: 2600 }
+        { x: new Date(2000,0), y: 1500 },
+        { x: new Date(2000,1), y: 1800 },
+        { x: new Date(2000,2), y: 2100 },
+        { x: new Date(2000,3), y: 1900 },
+        { x: new Date(2000,4), y: 2500 },
+        { x: new Date(2000,5), y: 2300 },
+        { x: new Date(2000,6), y: 2620 },
+        { x: new Date(2000,7), y: 2600 },
+        { x: new Date(2000,8), y: 3000 },
+        { x: new Date(2000,9), y: 2800 },
+        { x: new Date(2000,10), y: 3100 },
+        { x: new Date(2000,11), y: 3800 }
       ]
     }]
   });

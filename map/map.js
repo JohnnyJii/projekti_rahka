13b1c757befa21a4db711cb2d0e2a71f239fa05d
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2018-12-04 10:51:59 +0200

    route

--- map/map.js
+++ map/map.js
@@ -1,11 +1,11 @@
 var map = L.map('map');
 
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-  maxZoom: 15,
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  maxZoom: 20,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
   '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
   'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  id: 'mapbox.streets'
+  id: 'mapbox.streets',
 }).addTo(map);
 
 // placeholders for the L.marker and L.circle representing user's current position and accuracy
@@ -20,8 +20,7 @@ function onLocationFound(e) {
 
   var radius = e.accuracy / 75;
 
-  current_position = L.marker(e.latlng).addTo(map)
-  .bindPopup("You are here ");
+  current_position = L.marker(e.latlng).addTo(map).bindPopup('You are here ');
 
   current_accuracy = L.circle(e.latlng, radius).addTo(map);
 }
@@ -41,13 +40,14 @@ function locate() {
 // interval
 setInterval(locate, 2000);
 
-
 //Waypoints
-
 
 //Polyline
 var start = new L.LatLng(onLocationFound(e));
-var midway = new L.LatLng(onLocationFound(e));
+var midway = [];
+for (int i = 0, i < 720, i++)
+midway.add(onLocationFound()
+);
 var finish = new L.LatLng(onLocationFound(e));
 var pointList = [start, midway, finish];
 
@@ -55,7 +55,7 @@ var firstpolyline = new L.Polyline(pointList, {
   color: 'red',
   weight: 3,
   opacity: 0.5,
-  smoothFactor: 1
+  smoothFactor: 1,
 });
 firstpolyline.addTo(map);
 

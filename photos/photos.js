9c968b093e3c0dab31451e504990777088064cef
Ville Pellosniemi <villeope@metropolia.fi>
2018-12-04 11:50:27 +0200

    update

--- photos/photos.js
+++ photos/photos.js
@@ -55,19 +55,3 @@ const lahetaLomake = (evt) => {
 
 lomake.addEventListener('submit', lahetaLomake);
 
-let input = document.getElementById( 'file-upload' );
-let infoArea = document.getElementById( 'file-upload-filename' );
-
-input.addEventListener( 'change', showFileName );
-
-function showFileName( event ) {
-
-  // the change event gives us the input it occurred in
-  let input = event.srcElement;
-
-  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
-  let fileName = input.files[0].name;
-
-  // use fileName however fits your app best, i.e. add it into a div
-  infoArea.textContent = 'File name: ' + fileName;
-}

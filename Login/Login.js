d7abe7b1ee148044a6270768ee8e900691ac3934
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2018-12-04 10:22:36 +0200

    form

--- Login/Login.js
+++ Login/Login.js
@@ -41,3 +41,18 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
 
 });
+
+function validateForm(formId)
+{
+  var inputs, index;
+  var form=document.getElementById(formId);
+  inputs = form.getElementsByTagName('input');
+  for (index = 0; index < inputs.length; ++index) {
+    // deal with inputs[index] element.
+    if (inputs[index].value==null || inputs[index].value=="")
+    {
+      alert("Field is empty");
+      return false;
+    }
+  }
+}
